c2fef4c4f5e66da5395ef5e682251d3885a4a29d
mrahman <rahmanww@gmail.com>
2020-04-26 17:39:33 -0400

    adding module

---
+++ NYP/src/main/java/pages/menu/EntertainmentPage.java
@@ -0,0 +1,33 @@
+package pages.menu;
+
+import base.CommonAPI;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+
+public class EntertainmentPage {
+
+    private final WebDriver driver;
+
+    public EntertainmentPage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver,this);
+    }
+
+    @FindBy(how = How.CSS, using = "div.headline-wrapper a:nth-child(2) h3")
+    public static WebElement headLineNewsWebElement;
+
+    public WebElement getHeadLineNewsWebElement(){
+        return headLineNewsWebElement;
+    }
+
+    public void clickOnHeadLineNews(){
+        CommonAPI.waitUntilVisible(headLineNewsWebElement);
+        String headLineNews = getHeadLineNewsWebElement().getText();
+        System.out.println(headLineNews);
+        CommonAPI.waitUntilClickAble(headLineNewsWebElement);
+        getHeadLineNewsWebElement().click();
+    }
+}
